1fd52f27b96c5c7fb3cd2a62e128d262de7d06a6
Anaswar ranjith <90270760+ANASWARRANJITH@users.noreply.github.com>
2023-05-26 10:40:52 +0530

    Add files via upload

---
+++ age main.py
@@ -0,0 +1,7 @@
+age=int(input("enter the age::\n"))
+print(f"your are {age} years old")
+years_remaning=90-age
+days_remaining=years_remaning*365
+weeks_remaing=years_remaning*52
+months_remaing=years_remaning*12
+print(f"you have{days_remaining} days,{weeks_remaing}weeks,{months_remaing}left")
